eafd1975008a14aeeddaae76ca63eec5784dd031
nahjh2 <nahjh2@ajou.ac.kr>
2025-12-01 21:40:40 +0900

    add get method

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/user/:id', (req, res) => {
+    res.send(`User id is ${req.params.id}`)
+})
+
+app.get('/user', (req, res) => {
+    res.send(`User id is ${req.query.id}`)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
