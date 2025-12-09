2a6a4561e930773af4fedc8eeef200660c929570
HJH <nahjh2@gmail.com>
2025-12-09 16:00:53 +0900

    add firebase initialize error handling logic

--- firebase.js
+++ firebase.js
@@ -1,10 +1,17 @@
 var firebase = require("firebase-admin");
 
-var serviceAccount = require("./firebaseKey");
+try {
+  var serviceAccount = require("./firebaseKey");
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount)
-});
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+  });
+} catch (error) {
+  if (error.code === "MODULE_NOT_FOUND") {
+    console.warn(
+      `./firebaseKey 파일을 찾을 수 없습니다. Firebase Admin SDK 초기화를 건너뜁니다.`
+    );
+  }
+}
 
-
-module.exports = firebase
+module.exports = firebase;
